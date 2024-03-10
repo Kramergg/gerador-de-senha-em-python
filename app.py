6cd2bc71b1803f0d4d2d31ed70036c09f9bd2223
Gabriel Kramer <kramermota55@gmail.com>
2024-03-10 11:25:10 -0400

    update: modificamos a forma de gerar senha na função 'criar_nova_senha'

--- app.py
+++ app.py
@@ -1,44 +1,65 @@
+import random
+import string
 from colorama import init, Fore
 
+#  ||||||| COLORAMA |||||||
+
 # Inicializa o colorama para sistemas Windows
+class Color:
+    # Cores de texto
+    RED = '\033[91m'  # Vermelho
+    GREEN = '\033[92m'  # Verde
+    YELLOW = '\033[93m'  # Amarelo
+    BLUE = '\033[94m'  # Azul
+    MAGENTA = '\033[95m'  # Magenta
+    CYAN = '\033[96m'  # Ciano
+    WHITE = '\033[97m'  # Branco
+
+    # Formatação de texto
+    BOLD = '\033[1m'  # Negrito
+    UNDERLINE = '\033[4m'  # Sublinhado
+    END = '\033[0m'  # Resetar formatação
 init()
 
+#  ||||||| FUNÇÕES |||||||
+
 def criar_nova_senha():
-    print(" =================================================")
-    print(" ||             INSTRUÇÕES OPÇÃO 1:              ||")
+
+    # Define os conjuntos de caracteres para cada tipo de caractere
+    letras_maiusculas = string.ascii_uppercase
+    letras_minusculas = string.ascii_lowercase
+    numeros = string.digits
+    simbolos = '@#$%¨&*'
+
+    # Combina todos os conjuntos de caracteres
+    caracteres = letras_maiusculas + letras_minusculas + numeros + simbolos
+
+    # Gera uma senha aleatória com 12 caracteres
+    senha = ''.join(random.sample(caracteres, 15))
+    print(Color.BLUE + " =================================================")
+    print(" ||                INFORMAÇÃO:                   ||")
     print(" ||                                              ||")
     print(" ||      - Uma senha segura deve conter:         ||")
     print(" ||           -- Letra maiúscula                 ||")
     print(" ||           -- Letra minúscula                 ||")
-    print(" ||           -- números                         ||")
-    print(" ||           -- Simbolos (@#$%¨&*)              ||")
+    print(" ||           -- Números                         ||")
+    print(" ||           -- Símbolos (@#$%¨&*)              ||")
     print(" ||                                              ||")
-    print(" ==================================================")
-    password = input('Digite sua senha com base nos requisitos: ')
+    print(" =================================================="+ Color.END)
 
-    # Verifica o comprimento da senha
-    if len(password) < 8:
-        print('1 - Sua senha deve conter mais de 8 caracteres!')
+    print(Color.GREEN + " ==================================================")
+    print(" ||                                              ||")
+    print(" ||           SENHA GERADA COM SUCESSO!          ||")
+    print(" ||                                              ||")
+    print(" ||                 "+senha+"              ||")
+    print(" ||                                              ||")
+    print(" ||                                              ||")
+    print(" =================================================="+ Color.END)
 
-    # Verifica se a senha contém letras maiúsculas
-    if not any(char.isupper() for char in password):
-        print('2 - Sua senha deve conter pelo menos uma letra maiúscula!')
 
-    # Verifica se a senha contém letras minúsculas
-    if not any(char.islower() for char in password):
-        print('3 - Sua senha deve conter pelo menos uma letra minúscula!')
-
-    # Verifica se a senha contém números
-    if not any(char.isdigit() for char in password):
-        print('4 - Sua senha deve conter pelo menos um número!')
-
-    # Verifica se a senha contém símbolos
-    if not any(char in '@#$%¨&*' for char in password):
-        print('5 - 1Sua senha deve conter pelo menos um símbolo (@#$%¨&*)!')
- 
 
 def tratar_senha_fraca():
-    print(" =================================================")
+    print(Color.MAGENTA+" =================================================")
     print(" ||             INSTRUÇÕES OPÇÃO 2:              ||")
     print(" ||                                              ||")
     print(" ||   - Você deve colocar sua senha existente:   ||")
@@ -46,9 +67,11 @@ def tratar_senha_fraca():
     print(" ||                                              ||")
     print(" ||                                              ||")
     print(" ==================================================")
+    password = input( Color.BOLD +'Digite sua senha com base nos requisitos: ' + Color.END)
+    
 
 def verifica_senha_segura():
-    print(" =================================================")
+    print(Color.MAGENTA+" =================================================")
     print(" ||             INSTRUÇÕES OPÇÃO 3:              ||")
     print(" ||                                              ||")
     print(" ||   - Você deve colocar sua senha existente:   ||")
@@ -56,24 +79,16 @@ def verifica_senha_segura():
     print(" ||                                              ||")
     print(" ||                                              ||")
     print(" ==================================================")
+    password = input('Digite sua senha com base nos requisitos: ')
 
 
- 
+
 
 #  ||||||| MENU |||||||
 
 
 def menu():
-    print(" =================================================")
-    print(" ||                                             ||")
-    print(" ||   Selecione uma opção:                      ||")
-    print(" ||                                             ||")
-    print(" ||   1. Criar Nova Senha                       ||")
-    print(" ||   2. Tratar senha fraca deixando mais forte ||")
-    print(" ||   3. Verifica se a senha é segura ou não    ||")
-    print(" ||   0. Sair                                   ||")
-    print(" ||                                             ||")
-    print(" =================================================")
+
 
     while True:
         print(" =================================================")
@@ -86,7 +101,7 @@ def menu():
         print(" ||   0. Sair                                   ||")
         print(" ||                                             ||")
         print(" =================================================")
-        escolha = input("Digite o número da opção desejada: ")
+        escolha = input(Color.BOLD +"Digite o número da opção desejada: " + Color.END)
 
 
         if escolha == "1":
